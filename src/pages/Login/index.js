72529ee38674ee2a535e506b9f4e179599f629d8
Marcus Urani <marcus.urani20@gmail.com>
2021-12-21 17:15:20 -0300

    Criando contexts para feira e página de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,50 +1,46 @@
-import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
+import { Button } from "@material-ui/core";
+import { Container, Titulo, InputContainer } from "./styles";
+import { Input, InputLabel, InputAdornment } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { UsuarioContext } from "common/Context/Usuario";
+import { useContext } from "react";
 
 function Login() {
+  const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
   return (
     <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
+      <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
+        <InputLabel>Nome</InputLabel>
         <Input
           type="text"
+          value={nome}
+          onChange={(event) => {
+            setNome(event.target.value);
+          }}
         />
       </InputContainer>
       <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
+        <InputLabel>Saldo</InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          type="number"
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+          value={saldo}
+          onChange={(event) => {
+            setSaldo(event.target.value);
+          }}
+        />
       </InputContainer>
       <Button
         variant="contained"
         color="primary"
+        onClick={() => history.push("/feira")}
       >
         Avançar
       </Button>
     </Container>
-  )
-};
+  );
+}
 
 export default Login;
